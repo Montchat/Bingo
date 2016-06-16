a2e813f32af5d9deb69a2165ea15165c59b249d0
JoeE <JE4440@gmail.com>
2016-06-16 16:15:33 -0400

    bingo card will now sort

--- Bingo.playground/Contents.swift
+++ Bingo.playground/Contents.swift
@@ -2,18 +2,23 @@
 
 import Foundation
 
-class Bingo {
+class GameOfBingo {
     
     let players:[Player]!
     let gameType:GameType!
     let announcer:Announcer!
     let ballPit:BallPit!
     
+    var selectedBalls:[Ball] = []
+    var round:Int!
+    
     init(withPlayers players:[Player], gameType:GameType, announcer:Announcer, ballPit: BallPit!){
         self.players = players
         self.gameType = gameType
         self.announcer = announcer
         self.ballPit = ballPit
+        
+        round = 0
         
     }
     
@@ -30,30 +35,104 @@ enum GameType {
 
 class BingoCard {
     
+    enum Letter {
+        case B, I, N, G, O
+    }
+    
+    struct Square {
+        var number:Int!
+        var marked:Bool = false
+    }
+    
+    var grid:[Column] = []
+    
+    typealias Column = (Letter,[Square])
+    
+    var b:Column!
+    var i:Column!
+    var n:Column!
+    var g:Column!
+    var o:Column!
+    
     init() {
         
-    }
-    
-}
-
-typealias Name = String
-class Person {
-    
-    let name:Name!
-    init(withName name:Name) {
-        self.name = name
-    }
-    
-    func declareBingo() {
+        let freeSquare = Square(number: 0, marked:true)
         
-    }
-    
-}
-
-class Announcer {
-    
-    
-    init() {
+        self.b = (Letter.B, [])
+        self.i = (Letter.I, [])
+        self.n = (Letter.N, [freeSquare])
+        self.g = (Letter.G, [])
+        self.o = (Letter.O, [])
+        
+        var _b = Array(1...15)
+        var _i = Array(16...30)
+        var _n = Array(31...45)
+        var _g = Array(45...60)
+        var _o = Array(61...75)
+        
+        for _ in 1...5 {
+            
+            let ib = Int(arc4random_uniform(UInt32(_b.count)))
+            let ii = Int(arc4random_uniform(UInt32(_i.count)))
+            
+            let ig = Int(arc4random_uniform(UInt32(_g.count)))
+            let io = Int(arc4random_uniform(UInt32(_o.count)))
+            
+            let bSquare:Square = Square(number: _b[ib], marked: false)
+            let iSquare:Square = Square(number: _i[ii], marked: false)
+            let gSquare:Square = Square(number: _g[ig], marked: false)
+            let oSquare:Square = Square(number: _o[io], marked: false)
+            
+            _b.removeAtIndex(ib)
+            _i.removeAtIndex(ii)
+            _g.removeAtIndex(ig)
+            _o.removeAtIndex(io)
+            
+            self.b.1.append(bSquare)
+            self.i.1.append(iSquare)
+            self.g.1.append(gSquare)
+            self.o.1.append(oSquare)
+            
+            self.b.1.sortInPlace() {
+                $0.number < $1.number
+            }
+            
+            self.i.1.sortInPlace() {
+                $0.number < $1.number
+            }
+            
+            self.g.1.sortInPlace() {
+                $0.number < $1.number
+            }
+            
+            
+            self.o.1.sortInPlace() {
+                $0.number < $1.number
+            }
+        
+            
+        }
+        
+        for _ in 1...4 {
+            
+            let iN = Int(arc4random_uniform(UInt32(_n.count)))
+            
+            let nSquare:Square = Square(number: _n[iN], marked: false)
+            _n.removeAtIndex(iN)
+            
+            self.n.1.append(nSquare)
+            
+            self.n.1.sortInPlace() {
+                $0.number < $1.number
+            }
+            
+        }
+        
+        grid.append(b)
+        grid.append(i)
+        grid.append(n)
+        grid.append(g)
+        grid.append(o)l
         
     }
     
@@ -65,9 +144,7 @@ class Ball {
     
     let number:Number!
     
-    enum Letter {
-        case B, I, N, G, O
-    }
+    enum Letter { case B, I, N, G, O }
     
     let value:(Letter,Number)!
     
@@ -137,17 +214,41 @@ class Player:Person {
         self.card = card
         super.init(withName: name)
     }
-
+    
 }
 
-let card:BingoCard = BingoCard()
+typealias Name = String
 
-let joe = Player(withName: "Joe", andCard: card)
+class Person {
+    
+    let name:Name!
+    init(withName name:Name) {
+        self.name = name
+    }
+    
+    func declareBingo() {
+        
+    }
+    
+}
 
-joe.declareBingo()
-let ballPit = BallPit()
-let ball = ballPit.spitOutBall()
+class Announcer: Person {
+    
+    typealias Announcement = String
+    
+    func pickBallFrom(ballPit ballPit: BallPit, inout AddToSelectedBalls selectedBalls: [Ball]) -> Announcement {
+        
+        let pickedBall = ballPit.spitOutBall()
+        
+        let announcement:Announcement! = Announcement(pickedBall.value)
+        
+        selectedBalls.append(pickedBall)
+        
+        return announcement
+        
+    }
+    
+}
 
-
-
-
+let leCard = BingoCard()
+print(leCard.grid)
